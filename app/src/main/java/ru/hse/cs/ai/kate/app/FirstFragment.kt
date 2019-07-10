6765408c2f57b6e0fa548bfa0df32503fe4a4fcf
katsam <katsam@passap.ru>
2019-07-10 23:38:46 +0300

    Initial version with navigation between fragments using the menu.

---
+++ app/src/main/java/ru/hse/cs/ai/kate/app/FirstFragment.kt
@@ -0,0 +1,60 @@
+package ru.hse.cs.ai.kate.app
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.navigation.NavigationView
+
+import kotlinx.android.synthetic.main.fragment_first.*
+import kotlinx.android.synthetic.main.fragment_first.view.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [FirstFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [FirstFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class FirstFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+}
---
+++ app/src/main/java/ru/hse/cs/ai/kate/app/MainActivity.kt
@@ -0,0 +1,80 @@
+package ru.hse.cs.ai.kate.app
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.appcompat.widget.Toolbar as Toolbar1
+import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private val navController
+        get() = findNavController(R.id.nav_host_fragment)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(my_toolbar)
+        my_toolbar.setTitleTextColor(Color.WHITE)
+
+        setupActionBarWithNavController(navController, drawer_layout)
+        nav_view.setupWithNavController(navController)
+
+        nav_view.setNavigationItemSelectedListener {
+            val navTarget = when (it.itemId) {
+                R.id.action_addition_info -> R.id.addInfoFragment
+                R.id.action_contact_doctor -> R.id.doctorFragment
+                R.id.action_help -> R.id.helpFragment
+                R.id.action_notes -> R.id.notesFragment
+                R.id.action_profile -> R.id.profileFragment
+                R.id.action_reminder -> R.id.reminderFragment
+                R.id.action_settings -> R.id.settingsFragment
+                else -> null
+            }
+            if (navTarget != null) {
+                navController.navigate(navTarget)
+                drawer_layout.closeDrawer(GravityCompat.START)
+            }
+            false
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navigateUp(findNavController(R.id.nav_host_fragment), drawer_layout)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navTarget = when (item.itemId) {
+            R.id.action_settings -> R.id.settingsFragment
+            else -> null
+        }
+        if (navTarget != null) {
+            navController.navigate(navTarget)
+            drawer_layout.closeDrawer(GravityCompat.START)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
